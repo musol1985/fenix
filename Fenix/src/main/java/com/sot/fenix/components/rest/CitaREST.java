96eaf91aef36f34c74b8935fb35daf7099c18782
martine <martine@hcis>
2016-09-15 17:20:43 +0200

    x

--- Fenix/src/main/java/com/sot/fenix/components/rest/CitaREST.java
+++ Fenix/src/main/java/com/sot/fenix/components/rest/CitaREST.java
@@ -1,12 +1,19 @@
 package com.sot.fenix.components.rest;
 
+import java.util.Date;
+import java.util.List;
+
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sot.fenix.components.json.ResponseJSON;
+import com.sot.fenix.components.json.ResponseListJSON;
 import com.sot.fenix.components.models.Cita;
 import com.sot.fenix.components.services.CitaService;
 
@@ -24,6 +31,13 @@ public class CitaREST{
 		citas.getDAO().save(cita);
 		System.out.println(cita);
 		return new ResponseJSON<Cita>(ResponseJSON.OK, cita);	
+    }
+	
+	
+	@RequestMapping(method=RequestMethod.GET, path="/all/{fechaini}/{fechafin}/{centro}")
+    public ResponseListJSON<Cita> get(@PathVariable @DateTimeFormat(pattern="dd/MM/yyyy") Date fechaIni, @PathVariable @DateTimeFormat(pattern="dd/MM/yyyy") Date fechaFin, @PathVariable String centro) {
+		List<Cita> res=citas.getDAO().findByFechaIniGreaterThanEqualAndFechaFinLessThanEqualAndCentro_id(fechaIni, fechaFin, new ObjectId(centro));
+		return new ResponseListJSON<Cita>(ResponseJSON.OK, res);	
     }
 	
 }
--- Fenix/src/main/java/com/sot/fenix/dao/CitaDAO.java
+++ Fenix/src/main/java/com/sot/fenix/dao/CitaDAO.java
@@ -1,14 +1,18 @@
 package com.sot.fenix.dao;
 
+import java.util.Date;
+import java.util.List;
+
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.sot.fenix.components.models.Cita;
-import com.sot.fenix.components.models.Cliente;
 
 @Repository
 public interface CitaDAO extends MongoRepository<Cita, ObjectId>{	
 	//public Cliente findByCentro_idAndDni(ObjectId centro, String dni);
+	
+	public List<Cita> findByFechaIniGreaterThanEqualAndFechaFinLessThanEqualAndCentro_id(Date fechaIni, Date fechaFin, ObjectId centroId);
 
 }
