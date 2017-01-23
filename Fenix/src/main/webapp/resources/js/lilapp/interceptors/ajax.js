bc718f4ca8ff91a9d28d1c5f009d281161ab6ea3
eduarmar <eduarmar@XI14761892.emea.hpqcorp.net>
2017-01-23 17:46:25 +0100

    f

--- Fenix/src/main/webapp/resources/js/lilapp/interceptors/ajax.js
+++ Fenix/src/main/webapp/resources/js/lilapp/interceptors/ajax.js
@@ -8,21 +8,13 @@ materialAdmin
 	    			config.headers['lilap-is-ajax'] = true
 	    			return config;
 	    	    },	    
-	    	'response': function(response) {
-	    			console.log("onAjaxResponse"+response.status);
-	    			console.log(response.status);
-	    			if(response.status=="401"){
-	    				alert("Sesion expirada!");
-	    			}
-	    			 /*if (typeof response.data === 'string') {
-	    				 console.log("COmprobando fin de sesion!!!!!");
-	                    if (response.data.indexOf instanceof Function &&
-	                        response.data.indexOf('<html class="login-content" data-ng-app="materialAdmin">') != -1) {
-	                        $location.path("/logout");
-	                    }
-	                }  */     				
-	    		return response;
-	    	}
+	    	'responseError': function(rejection) {
+	            // do something on error
+	            if(rejection.status === 401){
+	            	alert("Sesion expirada!");                
+	            }
+	            return rejection;
+	         }
 		};
 	    
 	})
